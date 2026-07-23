381d3ae3c72183ee532312ad90e301052f93df4f
GOOGLE <foofaha0510@gmail.com>
2026-07-23 15:17:00 -0400

    Add files via upload

---
+++ Controller pcb stuff/TIMESAVE4HORIZONSPCBPROJECT_copy_20260723151635/TIMESAVE4HORIZONSPCBPROJECT_copy_20260723151635.ino
@@ -0,0 +1,7 @@
+void setup() {
+  // literally nothing, just need a code file to attach timelapses to so I can document time seperate from my outpost project.
+//my PCBs do have code, but that is asociated with my outpost project. my horizons project ONLY concerns my pcb design process.
+
+void loop() {
+  // 
+}
